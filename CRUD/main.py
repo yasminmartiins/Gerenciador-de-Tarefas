431f1ea33b35d3d5b675e7319319b020c197bb70
sthephany-souza <166942092+sthephany-souza@users.noreply.github.com>
2026-04-04 12:21:26 -0300

    Adicionado telas e login e cadastro

--- CRUD/main.py
+++ CRUD/main.py
@@ -150,5 +150,13 @@ def deletar_anexo(id):
             
     return jsonify({"success": True})
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastrar.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
